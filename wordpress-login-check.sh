4cbb1b442c6b5c6827cb5e581611212beba705f3
Lehman Black <dosnlinux@gmail.com>
2014-08-24 19:31:10 -0500

    minor changes

--- wordpress-login-check.sh
+++ wordpress-login-check.sh
@@ -1,24 +1,25 @@
-﻿#!/bin/bash
+#!/bin/bash
 
 # will also work in cygwin bash
-
-wppass=password
-wplogin=username
-site=www.example.com
-wproot=/wordpress
+#set -x
+#wpuser='ardent'
+#wppass='pav8c7xhwp'
+#wpsite='www.playafc.com'
+#wproot=/wordpress
 
 # wordpress first checks if cookies enabled by placing a test cookie
-curl --cookie-jar cookies.txt --output "tmp.html" "http://$site$wproot/wp-login.php"
+curl --cookie-jar cookies.txt -silent --output "tmp.html" "http://$wpsite$wproot/wp-login.php"
 
 # when logging on via the web page, after placing the test cookie wordpress will
 # redirect to wp-login.php again and then actually process the credentials to set
 # login cookies. This mimics the redirect.
-curl --cookie-jar cookies.txt --output "login.html" --max-redirs 0 --data "log=$wpuser&pwd=$wppass&rememberme=forever&redirect_to=http%3A%2F%2F$site$wproot%2Fwp-admin%2F&testcookie=1&wp-submit=Log%20In" "http://$site/wp-login.php"
+curl --cookie-jar cookies.txt -silent --output "login.html" --max-redirs 0 --data "log=$wpuser&pwd=$wppass&rememberme=forever&redirect_to=http%3A%2F%2F$wpsite$wproot%2Fwp-admin%2F&testcookie=1&wp-submit=Log%20In" "http://$wpsite/wp-login.php"
 
+# check for success entry in $COOKIEJAR
+grep -Fiq wordpress_logged_in cookies.txt 2> /dev/null
+LOGIN_SUCCESS=$?
 
-# if successful, the cookies.txt file should have:
-# 3 line comment from curl
-# 1 blank line
-# 1 line for the test cookie
-# other lines if wordpress authenticates
-[ `wc -l cookies.txt | cut -d ' ' -f1` -gt 5 ]
+# cleanup
+rm cookies.txt tmp.html login.html 2> /dev/null
+
+exit $LOGIN_SUCCESS
